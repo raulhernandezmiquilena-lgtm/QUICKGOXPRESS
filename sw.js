411744c0da7635b9dbea3656bd358bd3fd726b54
raulhernandezmiquilena-lgtm <raulhernandezmiquilena@gmail.com>
2026-09-04 22:40:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('cargaxpress-v1').then((cache) => {
-      return cache.addAll(['./', './index.html']);
-    })
-  );
-});
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
-});
