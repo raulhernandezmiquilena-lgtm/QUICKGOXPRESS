f2db8a70a691fd1fecdf326005c381dbcd9e74b9
raulhernandezmiquilena-lgtm <raulhernandezmiquilena@gmail.com>
2026-09-04 22:07:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('cargaxpress-v1').then((cache) => cache.addAll([
-      './',
-      './index.html'
-    ]))
+    caches.open('cargaxpress-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './invoice.css',
+        './invoice.js',
+        './manifest.json',
+        './tulogo.jpeg'
+      ]);
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
