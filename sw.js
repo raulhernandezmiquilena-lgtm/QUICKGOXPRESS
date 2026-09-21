6ea0daa7f5ebe87f618e7837f28a97d5a682e99e
raulhernandezmiquilena-lgtm <raulhernandezmiquilena@gmail.com>
2026-09-21 05:19:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+const CACHE_NAME = 'quickgoxpress-v1';
+const urlsToCache = [
+  './invoice.html',
+  './invoice.css',
+  './invoice.js',
+  './manifest.json'
+];
 
+// Instalación del Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Respuestas desde la caché o red
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
